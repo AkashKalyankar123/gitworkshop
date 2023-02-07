3c90edc2ee6e2be00e2ca5a0d51c06a000276d07
akashkalyankar123 <akashkalyankar05@gmail.com>
2023-02-07 10:17:24 +0530

    Added the Balanced Paranathsis...!

---
+++ src/BlancedParenthesis.java
@@ -0,0 +1,50 @@
+import java.util.Stack;
+
+public class BlancedParenthesis {
+
+	public static boolean isBalenced(String s)
+	{
+		Stack<Character> stack=new Stack<>();
+		
+		for(int i=0;i<s.length();i++)
+		{
+			char c=s.charAt(i);
+			
+			if(c == '(' || c == '{' || c == '[') {
+				
+				stack.push(c);
+				
+			}else if(c == ')' || c == '}' || c == ']')
+			{
+				if(stack.isEmpty())
+				{
+					return false;
+				}
+				char top=stack.pop();
+				
+				if(!((c == ')' && top == '(') || (c == '}' && top == '{') || (c == ']' && top == '[')))
+				{
+					return false;
+				}
+			}
+				
+		}
+		
+		return stack.isEmpty();
+		
+	}
+	
+	public static void main(String[] args) {
+		String s="(([()]))";
+		
+		if(isBalenced(s))
+		{
+			System.out.println(s +" is blanced Parenthesis expression.");
+		}
+		else
+		{
+			System.out.println(s +" is not blanced parenthesis expresiton.");
+		}
+	}
+
+}
